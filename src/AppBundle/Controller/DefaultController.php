af8273df2b7b958a23097659dd40c7df71b19ff4
Scato Eggen <scato.eggen@gmail.com>
2016-05-27 17:24:59 +0200

    Implement DefaultController (except for the page where you see the board)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use Application\StartGameCommand;
+use Ramsey\Uuid\Uuid;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,6 +13,7 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @Method("GET")
      */
     public function indexAction(Request $request)
     {
@@ -17,5 +21,33 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @Route("/start", name="start_game")
+     * @Method("POST")
+     */
+    public function startAction(Request $request)
+    {
+        $command = new StartGameCommand();
+        $command->gameId = Uuid::uuid4();
+        $command->playerName = $request->request->get('playerName');
+
+        $startGameHandler = $this->get('app.start_game_handler');
+        $entityManager = $this->get('doctrine.orm.default_entity_manager');
+
+        $startGameHandler->handle($command);
+        $entityManager->flush();
+
+        return $this->redirect($this->generateUrl('play_game', ['gameId' => $command->gameId->toString()]));
+    }
+
+    /**
+     * @Route("/play/{gameId}", name="play_game")
+     * @Method("GET")
+     */
+    public function playGame($gameId)
+    {
+
     }
 }
--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,17 +2,45 @@
 
 namespace Tests\AppBundle\Controller;
 
+use Application\StartGameCommand;
+use Application\StartGameHandler;
+use Doctrine\ORM\EntityManager;
+use Prophecy\Argument;
+use Prophecy\Prophet;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /**
+     * @test
+     */
+    public function shouldShowAStartButton()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+        $this->assertCount(1, $crawler->filter('form[action$="/start"][method="post"]'));
+        $this->assertCount(1, $crawler->filter('form[action$="/start"][method="post"] button'));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldStartAGame()
+    {
+        $prophet = new Prophet();
+        $startGameHandler = $prophet->prophesize(StartGameHandler::class);
+
+        $client = static::createClient();
+        $client->getKernel()->getContainer()->set('app.start_game_handler', $startGameHandler->reveal());
+
+        $client->request('POST', '/start');
+
+        $startGameHandler->handle(Argument::type(StartGameCommand::class))->shouldHaveBeenCalled();
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $this->assertContains('/play/', $client->getResponse()->headers->get('Location'));
     }
 }
